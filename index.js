27bac7f3653ef052112f55a1c3710ef68257c667
aliceauko <alice.auko@student.moringaschool.com>
2022-05-31 14:31:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello()
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  sayHelloToGuadalupe()
+
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  sayHelloToLiz()
+
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToSamip()
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
